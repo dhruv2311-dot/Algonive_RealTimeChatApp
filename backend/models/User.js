c282bdf28aece19d8cc300c10e5eb2fc88fc3f2d
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2025-12-15 09:23:02 +0530

    backend: add user model

---
+++ backend/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    endpoint: { type: String, required: true },
+    keys: {
+      p256dh: { type: String, required: true },
+      auth: { type: String, required: true }
+    }
+  },
+  { _id: false }
+);
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, select: false },
+    avatar: { type: String },
+    pushSubscriptions: [subscriptionSchema]
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = function comparePassword(candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+userSchema.methods.toJSON = function toJSON() {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export default mongoose.model('User', userSchema);
